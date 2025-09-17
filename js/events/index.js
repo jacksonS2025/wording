2218e495461803a913b6a2f43b6839356a44fb94
jacksonS2025 <jackson.shield264@education.nsw.gov.au>
2025-09-17 14:18:15 +1000

    make overflow hidden and auto scroll into view

--- js/events/index.js
+++ js/events/index.js
@@ -14,6 +14,11 @@ function buildWord(event) {
     document.querySelector('body').appendChild(letterTile)
     console.log('Finished creating tiles')
     console.log(letterTile)
+    letterTile.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center'
+    });
 }
 
 export function startListening() {
