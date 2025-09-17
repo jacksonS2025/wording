7cacdd13a36c42e0f85432e4f44e0f1f2a4b829f
jacksonS2025 <jackson.shield264@education.nsw.gov.au>
2025-09-17 14:06:10 +1000

    Create test

--- js/events/index.js
+++ js/events/index.js
@@ -3,6 +3,15 @@ import { verifyStarted } from "./verify.js"
 function buildWord(event) {
     console.log(verifyStarted())
     console.log(event.key)
+
+    // testing
+    const letterTile = document.createElement('div')
+    letterTile.classList.add('letterTile')
+    const letterTileText = document.createElement('p')
+    letterTileText.innerText = event.key;
+
+    letterTile.appendChild(letterTileText)
+    document.body.appendChild(letterTile)
 }
 
 export function startListening() {
